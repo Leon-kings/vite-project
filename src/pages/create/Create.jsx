d70a3625aa0cf29fae6449417f1971f74e08e971
Leon-kings <leonakingeneye2002@gmail.com>
2024-02-22 11:10:49 +0200

    chaged

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,8 +1,116 @@
 import React from 'react'
 import Screenshot from "../../assets/images/New folder/Screenshot.png"
   const Log =()=>{
+
+
+
+    const [values,setValues] = useState({
+ 
+      email:"",
+     
+      password:"",
+     
+     });
+     const Navigate =useNavigate();
+     const inputs=[
+     
+     
+      {
+        id:1,
+        name:"names",
+        type:"name",
+        placeholder:"Enter your name",
+        errorMessage:"Your names are needed",
+        label:"Yours names",
+        required:true
+      },
+     
+      {
+        id:2,
+        name:"password",
+        type:"password",
+        placeholder:"password",
+        errorMessage:"passworx should be 8-12 characters include at least 1 letter and 1 symbol",
+        label:"password",
+      
+        required:true
+      },
+     ];
+     
+     // fetching data from database
+           // handleSubmit
+     const handleSubmit = async(e) =>{
+      e.preventDefault();
+      // try{
+       
+      //   const dt = await axios.post('https://getcard.onrender.com/users/auth',values)
+      //   .then(() =>{
+      //     alert('User Logged in successfully');
+      //    Navigate('/Home')
+          
+      //   })
+      // }
+      //   catch(err){
+      //     console.log(err)
+      //   }
+     
+     }; 
+     
+     
+     
+     
+     const onChange =(e) =>{
+      setValues({...values,[e.target.name]:e.target.value});
+     }
+
+
+
   return (
    <>
+
+return(
+ <>
+  <p style={{visibility:"hidden"}}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos distinctio iste eius. Qui pariatur obcaecati quae doloribus tenetur cum ab autem, animi inventore accusantium assumenda quisquam eos magnam consectetur voluptatem.</p>
+ <div className="app">
+   <form onSubmit={handleSubmit}>
+     <h1>Log in</h1>
+     {inputs.map((input)=>(
+       <FormInput key={input.id}
+       {...input}value={values[input.name]}
+       onChange={onChange} />
+     ))}
+     <button className="btn">Submit</button>
+     <p>If you don`t have an account got <a ><Link to="/Register"><b>Register</b> </Link> </a></p>
+     
+   </form>
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 <h2 style={{visibility:"hidden"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ab, illum iusto optio rem eius atque est ea quibusdam repudiandae magnam? Quibusdam, suscipit nisi molestias nobis libero quidem distinctio enim. </h2>
 
